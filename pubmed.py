b6e9b5d4fcbedcccfcf82e0bc7a7dd11c3c73f57
Shachi Mittal <shachimittal@wirelessprv-10-193-223-97.near.illinois.edu>
2017-07-07 15:17:28 -0500

    pubmedID search

--- pubmed.py
+++ pubmed.py
@@ -1,11 +1,12 @@
-# query the pubmed database 
+# query the pubmed database
+from Bio import Entrez 
 def search(query):
 	Entrez.email = 'shachi.iitd@gmail.com'
-	handle = Entrez.esearch(db='pubmed',sort='relevance',retmax='20',retmode = 'xml',term=query)
+	handle = Entrez.esearch(db='pubmed',sort='relevance',retmax='20',retmode = 'json',term=query)
 	results = Entrez.read(handle)
 	return results
 
-park = search('parkinson disease)
+park = search('parkinson disease')
 Park_ID = park['IdList']
 
 
